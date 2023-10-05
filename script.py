ba112cc6580a0f3afe5768b4d1534e09cd9af432
David Kiss <davidkiss83@gmail.com>
2023-10-05 16:00:43 +0200

    exploration

--- script.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def imports():
-    data_train = pd.read_csv('data/train_values.csv')
-    data_labels = pd.read_csv('data/train_labels.csv')
-    data_test = pd.read_csv('data/test_values.csv')
