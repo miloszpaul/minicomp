4ebf2c7cbc7523270335cbd01e886c0c3f131034
CB Niederreuther <135853799+cbn000@users.noreply.github.com>
2023-10-06 09:31:31 +0200

    Script generation and addition to main Moved tree generation into new script Created/renamed helper script Updated main

---
+++ helper_functions.py
@@ -0,0 +1,29 @@
+### Helper functions
+# Last edit: 06.10.2023 08:58
+
+import pandas as pd
+
+def imports() -> tuple:
+    """Load data from csv files
+
+    Returns:
+        tuple: Tuple of 3 DataFrame: X, Y, X_test
+    """
+    X = pd.read_csv('data/train_values.csv')
+    Y = pd.read_csv('data/train_labels.csv')
+    X_test = pd.read_csv('data/test_values.csv')
+    return X, Y, X_test
+    
+    
+def write_output(X_test, y_pred) -> None:
+    """Create output.csv following drivendata.com submission standards and write file to disk
+
+    Args:
+        X_test (pd.Dataframe): Test dataset, x
+        y_pred (pd.Dataframe): Predictions, y
+    """
+    # Create Dataframe for output
+    output_df = pd.DataFrame({'building_id': X_test['building_id'], 'damage_grade': y_pred})
+    
+    # Write to csv
+    output_df.to_csv('output.csv', index=False)
---
+++ lgb_optimized.py
@@ -0,0 +1,131 @@
+# Class for random forest including optimizer
+# Strongly modified from original source: ChatGPT
+# Last edit: 06.10.2023 07:20
+
+import lightgbm as lgb
+import optuna
+import pandas as pd
+from sklearn.metrics import f1_score, make_scorer
+
+
+class LGBM():
+    """Class for LightGBM Forest and Optuna optimizer
+    """
+    
+    def __init__(self, X_train, X_valid, y_train, y_valid) -> None:
+        """Init class, store training and validation sets. Modify data where needed
+
+        Args:
+            X_train (pd.DataFrame): Training set, x values
+            X_valid (pd.DataFrame): Validation set, x values
+            y_train (pd.DataFrame): Training set, y values
+            y_valid (pd.DataFrame): Validation set, y values
+        """
+        # F1 score tracker. Use f1 micro for drivendata competition
+        self.f1_micro_scorer = make_scorer(f1_score, average='micro')
+        
+        # Initialize the best_f1_score variable as a list
+        self.best_f1_score = [0.0]
+        
+        # For optimization, classes need to go from [0,x]. Dataset starts at 1. Make adjustment
+        self.y_train = y_train - 1
+        self.y_valid = y_valid - 1
+        
+        # No change at X
+        self.X_train = X_train
+        self.X_valid = X_valid
+        
+
+    def _objective(self, trial) -> float:
+        """Set parameters for optimization, keep track of f1"""
+    
+        params = {
+            'objective': 'multiclass',
+            'num_class': 3,  # Set the number of classes in your dataset
+            'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3),
+            'subsample': trial.suggest_float('subsample', 0.7, 1.0),
+            'num_leaves': trial.suggest_int('num_leaves', 10, 100),
+            'min_data_in_leaf': trial.suggest_int('min_data_in_leaf', 1, 20),
+            'max_depth': trial.suggest_int('max_depth', 3, 15),
+            'lambda_l2': trial.suggest_float('lambda_l2', 0, 1),
+            'verbose': -1  # Suppress LightGBM output,
+        }
+
+        # Create the LightGBM dataset
+        train_data = lgb.Dataset(self.X_train, label=self.y_train)
+        valid_data = lgb.Dataset(self.X_valid, label=self.y_valid, reference=train_data)
+
+        # Train the multiclass classification model without early stopping
+        model = lgb.train(
+            params,
+            train_data,
+            valid_sets=[valid_data],
+            num_boost_round=1000,  # You can adjust this as needed
+        )
+
+        # Predict probabilities for each class on the validation set
+        y_pred_prob = model.predict(self.X_valid, num_iteration=model.best_iteration)
+
+        # Determine the predicted class by selecting the one with the highest probability
+        y_pred = y_pred_prob.argmax(axis=1)
+
+        # Adjust the predicted class labels back to the original range of 1 - 3
+        self.y_pred_original_range = y_pred + 1
+
+        # Calculate F1 score using micro averaging (Drivendata.com uses F1 micro)
+        f1 = f1_score(self.y_valid, self.y_pred_original_range, average='micro')
+        
+        # Update the best F1 score if a better score is found
+        if f1 > self.best_f1_score[0]:
+            self.best_f1_score[0] = f1
+        
+        # Perform early stopping manually
+        if trial.should_prune():
+            raise optuna.TrialPruned()
+        
+        return f1
+    
+    def optimization(self, X, y, X_test) -> pd.DataFrame:
+        """Run optimization. Build trees, tweak hyperparameters and return prediction of test set
+
+        Args:
+            X (pd.DataFrame): Full dataset (training and validation), x values
+            y (pd.DataFrame): Full dataset (training and validation), y values
+            X_test (pd.DataFrame): Testset, x values
+
+        Returns:
+            pd.DataFrame: Prediction of y values on testset
+        """
+            
+        study = optuna.create_study(direction='maximize', sampler=optuna.samplers.TPESampler())  # Maximize F1 score
+        study.optimize(self._objective, n_trials=100)
+        
+        # Info on current run
+        print('Number of finished trials:', len(study.trials))
+        print('Best trial:')
+        trial = study.best_trial
+
+        #print('  Value: {:.3f}'.format(trial.value))
+        #print('  Params: ')
+        #for key, value in trial.params.items():
+        #    print('    {}: {}'.format(key, value))
+        
+        # store best parameter set    
+        best_params = trial.params
+        
+        # Lovely ASCII line, report best f1 score
+        print("--------------------------------")
+        print(f"Best F1 Score: {self.best_f1_score[0]}")
+        print("--------------------------------")
+
+
+        self.final_model = lgb.LGBMClassifier(
+            objective='multiclass',
+            **best_params
+        )
+
+        # fit model on full dataset
+        self.final_model.fit(X, y)
+        
+        return self.final_model.predict(X_test)
+            
